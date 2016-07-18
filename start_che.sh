ef027487981ef4e0993a53b61e880595ddd24772
alexpud <alexandre.maciele@gmail.com>
2016-07-18 14:23:42 -0300

    Added user on che configuration file, enabling nginx to execute bash scripts. It couldn't execute them before, because it did not have permission to do so. Comments were added to start_che.sh file, and also was reorganized.

--- start_che.sh
+++ start_che.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-#THe first parameter,for now, is the name of the user.
+#------------------------------------First parameter,for now, is the name of the user-----------------------#
 
 if test -z $1 ; then
 	echo "Failed. No user was entered"
@@ -13,39 +13,52 @@ else
 fi
 
 
-#Obtains the machine IPv4 address
-MACHINE_IP=$(ip route get 1 | awk '{print $NF;exit}')
+#------------------------------------Paths used in the script----------------------------------------------#
 
-echo $MACHINE_IP
+SCRIPTS_PATH='/home/boss/nginx_scripts' 
+CHE_PATH='/home/boss/Desktop/eclipse-che-4.0.1'
+
+#----------------------------------------------------------------------------------------------------------#
+
+
+#------------------------------------Obtains the IPV4 address----------------------------------------------#
+function getIP
+{
+	MACHINE_IP=$(ip route get 1 | awk '{print $NF;exit}')
+}
+
+#----------------------------------------------------------------------------------------------------------#
+
+
+
+#-----------------------------------Obtains the port values------------------------------------------------#
+function setPorts
+{
+	INC=$(cat $SCRIPTS_PATH/data.txt) #Reads the increment value
+	CHE_PORT=$(expr 8080 + $INC) #New Che port
+	TOMCAT_SERVER_PORT=$(expr 8005 + $INC) #New Tomcat port
+	
+	#Sets the new increment value on the txt file
+	echo $(expr $INC + 1) > $SCRIPTS_PATH/data.txt
+}
+
+#----------------------------------------------------------------------------------------------------------#
+
+getIP
+echo $SCRIPTS_PATH
+setPorts
 
 #Reloads the environment, changing the environment variables values, more specifically, java path.
 
-source /etc/environment
+. /etc/environment
 
-#Path to working che directory
-DIRECTORY='/home/boss/Desktop/eclipse-che-4.0.1'
+#This command edits the port attribute on the Server tag on eclipse che tomcat
 
-#Reads the number inside the file that dictates the value of the increment
-INC=$(cat /home/boss/nginx_scripts/data.txt)
-
-#The port for the che instance will be the increment added to the default port value, 8080
-CHE_PORT=$(expr 8080 + $INC)
-
-#The second port will be for the tomcat server
-TOMCAT_SERVER_PORT=$(expr 8005 + $INC)
-
-echo $TOMCAT_SERVER_PORT
-
-#Writes the modified inc value back to the textfile, overwriting it's contents
-echo $(expr $INC + 1)>/home/boss/nginx_scripts/data.txt
-
-#This command edits the port attribute on the Server tag on the file containerd on eclipse-che-version/tomcat/conf/server.xml
-
-xmlstarlet edit -L -u "/Server/@port" -v "$TOMCAT_SERVER_PORT" $DIRECTORY/tomcat/conf/server.xml
+xmlstarlet edit -L -u "/Server/@port" -v "$TOMCAT_SERVER_PORT" $CHE_PATH/tomcat/conf/server.xml
 
 
-#Executes the che instance for the ip address and port especified
-$DIRECTORY/bin/che.sh run -r:192.168.25.6 -p:$CHE_PORT
+#-----------------------------------Executes the che instance for the ip address and port especified-------#
+$CHE_PATH/bin/che.sh run -r:$MACHINE_IP -p:$CHE_PORT
 
 #sed -i -- 's/<Server port="8005" shutdown="SHUTDOWN">/<Server port="'"$tomcat_server_port"'" shutdown="SHUTDOWN">/g' /home/boss/nginx_scripts/#teste.xml
 

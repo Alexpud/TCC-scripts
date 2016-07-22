a6740e874d7d40a8a5035ad6087559ba69e83b00
alexpud <alexandre.maciele@gmail.com>
2016-07-21 22:45:08 -0300

    Modifying che directory script done

--- modifica_arquivo.sh
+++ modifica_arquivo.sh
@@ -2,4 +2,22 @@
 
 #Usei o seguinte comando para modificar o padrao /primeiro/segundo
 
-sed -i 's/\/primeiro/\/segundo\/terceiro/g' data.txt
+#Takes the parameter, and searchs for the pattern: /workspaces/$1. When the pattern is found, it is replaced by: /workspaces
+
+function reset_workspace()
+{
+	sed -ri 's/\/workspaces\/\w+/\/workspaces/g' che.properties
+}
+
+function set_workspace()
+{
+	sed -i 's/\/workspaces/\/workspaces\/'$1'/g' data.txt
+}
+
+#reset_workspace "alexandre"
+
+#I managed to make a regex that searched for the pattern: com/(anyword) with the following regex: /com\/(a-z)\w+. So i adapted it to what i needed, including \w+ to the regex i already had. It didn't work. I searched a little and found that i need to add the -r option to sed, for the extended regex and it worked like a charm.
+
+sed -ri 's/\/workspaces\/\w+/\/workspaces/g' che.properties
+
+#sed -i 's/\/workspaces/\/segundo\/'$a'/g' che.properties

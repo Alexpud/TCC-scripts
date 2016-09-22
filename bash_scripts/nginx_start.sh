84f2d2beee8e2ec8b61c77814cf9e4041736c6c1
alexandre <alexandre.maciele@gmail.com>
2016-09-21 21:52:12 -0300

    Started adding ssl to nginx

--- bash_scripts/nginx_start.sh
+++ bash_scripts/nginx_start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Creates the socket for nginx
 sudo nginx -p `pwd`/ -c /usr/local/openresty/nginx/conf/nginx.conf
---
+++ bash_scripts/socket.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/local/openresty/nginx/lib/sockproc-master/sockproc /tmp/nginx.sock
+sudo chmod 666 /tmp/nginx.sock
